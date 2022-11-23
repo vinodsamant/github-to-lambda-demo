b8dd77aa713528634310d2672930fb8fa8f0bd29
vinodsamant <vinod.samant.123@gmail.com>
2022-11-23 11:12:51 +0530

    update 8

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,7]}
+    d = {'col1': [1,2], 'col2': [3,8]}
     df = pd.DataFrame(data=d)
     print(df)
     print('Done x1.1')
