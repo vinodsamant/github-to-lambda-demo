4625ddfc945fe7f27c639d2430ef415db1c803c5
vinodsamant <vinod.samant.123@gmail.com>
2022-11-25 17:35:58 +0530

    updated sleep command

--- lambdafunctionstatus-uat.sh
+++ lambdafunctionstatus-uat.sh
@@ -4,5 +4,6 @@ echo -e "Checking status of lambda code."
 command=`aws lambda get-function --function-name mylambdafunction-uat --query 'Configuration.LastUpdateStatus'| cut -b 2-11 `
 while [ "$command" != "Successful" ]
 do 
+   sleep 2
    echo "$command"
 done
