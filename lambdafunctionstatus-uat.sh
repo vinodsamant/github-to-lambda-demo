fb85823739a52b0365ef9218c1810ff68e9cec6a
vinodsamant <vinod.samant.123@gmail.com>
2022-11-25 21:00:33 +0530

    up

--- lambdafunctionstatus-uat.sh
+++ lambdafunctionstatus-uat.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo -e "Checking status of lambda code."
-command=`InProgess`
-while [ "$command" != "Successful" ]
-do 
-   command=`aws lambda get-function --function-name mylambdafunction-uat --query 'Configuration.LastUpdateStatus'| cut -b 2-11`
-   echo $command
+while :
+do
+	command=`aws lambda get-function --function-name mylambdafunction-uat --query 'Configuration.LastUpdateStatus'| cut -b 2-11`
+        read command
+	if [ "$command" != "InProgress" ]
+	then
+		break
+	fi
+	
+	echo $command
+   
 done
