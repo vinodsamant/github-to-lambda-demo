dd01a7882fcfbf6dcdb56d96228bfd9ed611ffd9
vinodsamant <vinod.samant.123@gmail.com>
2022-11-28 15:54:16 +0530

    updated script

--- lambdafunctionstatus.sh
+++ lambdafunctionstatus.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-
-#echo -e "Checking status of lambda code."
-#command=`aws lambda get-function --function-name mylambdafunction-dev --query 'Configuration.LastUpdateStatus'| cut -b 2-11 `
-#while [ "$command" != "Successful" ]
-#do 
-#   echo "$command"
-#done
-
-#!/bin/bash
-
-#Updated
  
+ ## For Status Check of Lambda Function
 while :
 do
 	command=`aws lambda get-function --function-name $Lambda_Function --query 'Configuration.LastUpdateStatus'| cut -b 2-11`
